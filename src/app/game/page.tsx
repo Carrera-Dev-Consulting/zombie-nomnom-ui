0cac86e8ceb6a56cd1a7a4ab1bc7ea57d4779978
Andres Hermilo Carrera <andreshcar@live.com>
2024-10-30 21:17:00 -0600

    Saving everything

---
+++ src/app/game/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Game() {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            
+            <h1>Last Round Starts when the first player reaches 13pts or More!!</h1>
+            <div></div>
+        </div>
+    );
+}
