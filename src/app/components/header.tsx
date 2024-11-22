ea514424216345c71538b77c2ef15f28287c5ad5
vCertxfiedGoat <140995210+vCertxfiedGoat@users.noreply.github.com>
2024-11-21 22:48:18 -0700

    feat(UI):Added the header component and added jest to test this component (#15)
    ## Describe your changes
    Added navigation bar and jest to test it
    ## Issue ticket number and link
    https://github.com/Carrera-Dev-Consulting/zombie-nomnom-ui/issues/10
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [x] I have defined what tests cover my change and or added new test to
    cover change.
    - [x] I have added comments to the change and updated any relevant docs.
    - [x] I have pulled the latest in the main branch and merged them ahead
    of time with no conflicts.

---
+++ src/app/components/header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+interface HeaderProps {
+  title: string;
+  links: ListItemProps[];
+}
+
+interface ListItemProps {
+  text: string;
+  url: string;
+}
+
+export default function Header(props: HeaderProps) {
+  return (
+    <nav className="flex bg-neutral-800 flex-row text-white py-3 w-full">
+      <h1 className="flex-row font-medium text-4xl ml-4 ">
+        <Link href="/">{props.title}</Link>
+      </h1>
+      <ul className="flex-2 flex text-2xl items-end ml-auto mr-4">
+        {props.links.map((link) => {
+          return <ListItem key={link.text} text={link.text} url={link.url} />;
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+function ListItem(props: { text: string; url: string }) {
+  return (
+    <li className="mx-3 text-black bg-white rounded px-5 hover:bg-neutral-600 hover:text-white">
+      <Link href={props.url}>{props.text}</Link>
+    </li>
+  );
+}
---
+++ test/component/header.test.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Header from "../../src/app/components/header";
+import { describe, expect, it } from "@jest/globals";
+import { render } from "@testing-library/react";
+
+describe("Header Component", () => {
+  it("Header should render", async () => {
+    const wrapper = render(<Header title="test" links={[]} />);
+    expect(await wrapper.findByText("test")).toBeTruthy();
+  });
+});
