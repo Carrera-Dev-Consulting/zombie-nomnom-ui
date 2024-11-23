762099454126c20371bd3c1fd263db0110c6b4ab
Alexander Quintanilla <140995210+vCertxfiedGoat@users.noreply.github.com>
2024-11-23 10:53:52 -0700

    updated import calls for header move

--- tests/component/header.test.tsx
+++ tests/component/header.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Header from "../../src/app/components/header";
+import Header from "../../src/app/components/header/header";
 import { describe, expect, it } from "@jest/globals";
 import { render } from "@testing-library/react";
 
